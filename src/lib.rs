17580e14e5b54e645508a8cb21456a8cfb5c6760
Alex Holmes <alex@sed.dev>
2024-11-10 15:48:02 -0500

    Basic impl

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::cmp::max;
+
+struct HyperLogLog {
+    registers: Vec<u8>,
+    hasher: DefaultHasher,
+}
+
+impl HyperLogLog {
+    pub fn new() -> Self {
+        Self {
+            registers: vec![0; 1 << 8],
+            hasher: DefaultHasher::new(),
+        }
+    }
+
+    pub fn evaluate(&self) -> f64 {
+        let sum: f64 = self.registers
+            .iter()
+            .map(|&x| x as f64)
+            .sum();
+        sum / (1 << 8) as f64
+    }
+
+    pub fn insert(&mut self, elem: &[u8]) {
+        elem.hash(&mut self.hasher);
+        let h = self.hasher.finish();
+        let r = ((0xFF & h) >> 56) as usize;
+        self.registers[r] = max(self.registers[r], h.leading_zeros() as u8);
+    }
+
+    pub fn merge(&mut self, other: &Self) {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert() {
+        let mut hll = HyperLogLog::new();
+        hll.insert("test1".as_bytes());
+        println!("{}", hll.evaluate())
+    }
+}
