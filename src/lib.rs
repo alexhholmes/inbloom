f3a69d326150fff67e2390a0abccca780611df0c
Alex Holmes <alex@sed.dev>
2024-11-10 21:22:55 -0500

    Check if R is valid

--- src/lib.rs
+++ src/lib.rs
@@ -53,6 +53,10 @@ impl<H: Hasher + Default, const R: u8> HyperLogLog<H, R> {
             }
         }
     }
+
+    const fn assert_valid_r() {
+        let _ = [(); (R < 17) as usize - 1];
+    }
 }
 
 impl Default for HyperLogLog<DefaultHasher, 8> {
